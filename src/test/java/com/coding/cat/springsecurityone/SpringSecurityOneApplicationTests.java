3388c4e8a962b0aad01137cc95e3c1e2029b1823
jhg097 <jhg097@naver.com>
2023-08-10 20:00:37 +0900

    환경설정

---
+++ src/test/java/com/coding/cat/springsecurityone/SpringSecurityOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.cat.springsecurityone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
